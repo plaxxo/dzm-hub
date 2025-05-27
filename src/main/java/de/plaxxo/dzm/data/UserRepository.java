4c22bdcf7c87f3ae2ba511f0a9f42c876f24a08e
Oli Hahn <oli.hahn3@gmail.com>
2025-05-27 12:25:05 +0200

    Switch to MongoRepository for UserRepo

--- src/main/java/de/plaxxo/dzm/data/UserRepository.java
+++ src/main/java/de/plaxxo/dzm/data/UserRepository.java
@@ -1,9 +1,20 @@
 package de.plaxxo.dzm.data;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import java.util.List;
 
-public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
-
+public interface UserRepository extends MongoRepository<User, String> {
+    
     User findByUsername(String username);
+    
+    // Benutzerdefinierte Abfragen mit der MongoDB-Abfragesyntax
+    @Query("{ 'roles': { $in: ['ADMIN'] } }")
+    List<User> findAllAdmins();
+    
+    // Benutzerdefinierte Projektion (nur bestimmte Felder)
+    @Query(value = "{ }", fields = "{ 'username': 1, 'name': 1 }")
+    List<User> findAllUserBasicInfo();
+    
+    boolean existsByUsername(String username);
 }
